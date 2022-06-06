3321a3393aa22a98f6cfdcf801cea96c89413992
Emmanuel Ani <106850197+emmanuelani@users.noreply.github.com>
2022-06-06 13:44:38 +0100

    Added a comment to explain what "lt" is

--- infinite_loop.sh
+++ infinite_loop.sh
@@ -6,7 +6,7 @@
 num=2
 word='This would continue running till infinity'
 
-while [ $num -lt 5 ]
+while [ $num -lt 5 ] # lt means less than, it is used to compare values
 do
     echo $word seq 1 90
 done
